e029be85696580f72cfc133e7c357964f67f1aab
Allen Hodoameda Kojo Mawuli Mensah <128984966+xmawulee@users.noreply.github.com>
2026-07-24 16:59:45 +0000

    Delete msg.ps1

--- msg.ps1
+++
@@ -1,8 +0,0 @@
-$content = Get-Content $args[0]
-$newContent = @()
-foreach ($line in $content) {
-    $newLine = $line -replace '^first commit\s*$', 'Admin'
-    $newLine = $newLine -replace '^Commit Admin files\s*$', 'Admin files'
-    $newContent += $newLine
-}
-Set-Content -Path $args[0] -Value $newContent
